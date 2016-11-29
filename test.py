f84158a6f5663e26534ff3f5b180dcc72ba61fde
yxy2529023446 <yxy2529023446@icloud.com>
2016-11-29 15:30:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-#插入U盘（多个），运行脚本，自动执行文件操作
+#运行脚本，插入U盘（只能插单个），自动执行文件操作，执行完之后退出
 
-import wmi
-
+import wmi		
+		
 #找到所有U盘，把U盘信息放在字典里{盘符：U盘名}，返回字典			
 def usb_info():
 	c = wmi.WMI()
@@ -21,8 +21,12 @@ def usb_info():
 def action():
 	dic = usb_info()
 	for key in dic:
-		file = open(key + r'\test.txt')
-		print(file.read())
+		with open(key + r'\test.txt') as file:
+			print(file.read())	
+	if dic == {}:
+		return 0
+	else:
+		return 1
 
 """测试，打印出U盘信息	
 def test():
@@ -30,5 +34,12 @@ def test():
 	disks = list(c.Win32_LogicalDisk(DriveType=2))
 	print(disks[1])"""
 	
+def main():
+	while True:
+		print('waitting...')
+		if action():
+			print('--finished--')
+			break
+			
 if __name__ == '__main__':
-	action()
+	main()
